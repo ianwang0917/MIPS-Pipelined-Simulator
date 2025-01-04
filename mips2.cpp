a1277645fcf603a65dd6e2a7e8e6f3467f9cde9c
ianwang0917 <ianwang0917@gmail.com>
2025-01-04 16:05:05 +0800

    edit: Instruction vector changed to queue, moved to class inside.

--- mips2.cpp
+++ mips2.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
 #include <fstream>
-#include <string>
+#include <cstring>
+#include <sstream>
 #include <bitset>
 #include <vector>
+#include <queue>
 using namespace std;
 
 struct IFStruct { //  Instruction fetch from memory
+    string instruction; // Full instruction. e.g. "lw $2, 8($0)"
     bitset<32> PC;
     bool nop = false;
 };
 
 struct IDStruct { // Instruction decode & register read
-    bitset<32> Instruction;
+    string Op; // Split instruction. e.g. "lw", "sw", "add", "sub", "beq
+    bitset<5> Rs;
+    bitset<5> Rt;
+    bitset<5> Rd;
+    bitset<16> Immediate;
     bool nop = false;
 };
 
 struct EXStruct { //Execute operation or calculate address
+    string Instruction;
     bitset<5> Rs;
     bitset<5> Rt;
     bitset<5> Rd;
@@ -34,6 +42,7 @@ struct EXStruct { //Execute operation or calculate address
 };
 
 struct MEMStruct { // Access memory operand
+    string Instruction;
     bitset<32> ALUResult;
     bool MemRead = false; // Used in MEM
     bool MemWrite = false; // Used in MEM
@@ -44,6 +53,7 @@ struct MEMStruct { // Access memory operand
 };
 
 struct WBStruct { // Write result back to register
+    string Instruction;
     bitset<5> Rs;
     bitset<5> Rt;
     bool RegDst = false; // Used in WB
@@ -59,13 +69,34 @@ private:
     EXStruct EX;
     MEMStruct MEM;
     WBStruct WB;
+    int registers[32] = {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+    int memory[32] = {1};
+    queue<string> instructions;
 public:
     void excuteIF() {
-
+        IF.instruction = instructions.front();
+        instructions.pop();
     }
 
     void excuteID() {
+        stringstream ss(IF.instruction);
+        ss >> ID.Op; // stringstream split string by space in default.
+        if(ID.Op == "lw") {
+            ss >> ID.Rs;
+            ss.ignore();
+            ss >> ID.Rt;
+        } else if(ID.Op == "sw") {
+            
+        } else if(ID.Op == "add") {
 
+        } else if(ID.Op == "sub") {
+
+        } else if(ID.Op == "beq") {
+
+        } else {
+            throw "Unknown instruction!";
+        }
+        cout << "123";
     }
 
     void excuteEX() {
@@ -92,24 +123,26 @@ public:
 
     }
 
-};
-
-vector<string> instructions = {
-    "lw $2, 8($0)"
-};
-
-void readInstruction() {
-    fstream file;
-    file.open("inputs/test3.txt");
-    if(!file) {
-        throw "Can't open file";
+    void readInstructions() {
+        fstream file;
+        file.open("inputs/test1.txt");
+        if(!file){
+            throw "Can't open file";
+        }
+        string instruction;
+        while(getline(file, instruction)){
+            instructions.push(instruction);
+        }
+        file.close();
     }
-    string instruction;
-    while(getline(file, instruction)) {
-        instructions.push_back(instruction);
+
+    void run() {
+        readInstructions();
+        excute();
     }
-}
+};
 
 int main(void) {
-    //readInstruction();
+    Pipeline pipeline;
+    pipeline.run();
 }
